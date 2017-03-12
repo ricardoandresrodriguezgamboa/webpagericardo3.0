1a171421b6c6f895138599e671cfec42a7e3f327
Ricardo Rodriguez <ricardoandres749@gmail.com>
2017-03-12 19:49:09 -0300

    ir-arriba

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,32 @@
+$(function(){
+     
+          
+  
 
-$(document).ready(function(){
- 
+        var header = document.getElementById('header');
+        
+        var headroom  = new Headroom(header);
+        
+        headroom.init(); 
+
+
 });
+   
+    $(document).ready(function(){
+        $('.ir-arriba').click(function(){
+            $('body,html').animate({
+                scrollTop: '0px'
+            },500);
+        });
 
-
+        $(window).scroll(function(){
+                if($(this).scrollTop() > 0){
+                      $('.ir-arriba').slideDown(300);
+                }else{
+                      $('.ir-arriba').slideUp(300);
+                }
+        });
+    });
 
     $(document).on("scroll", onScroll);
     
---
+++ js/waypoints.js
@@ -0,0 +1,9 @@
+var $dipper = $('.dipper');
+$dipper.waypoint(function(){
+   $dipper.addClass('dipper-animation');
+},{offset:'80%'});
+
+var $dipper2 = $('.dipper2');
+$dipper2.waypoint(function(){
+   $dipper2.addClass('dipper-animation2');
+},{offset:'90%'});
