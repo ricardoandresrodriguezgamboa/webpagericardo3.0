9a1cb1a5a395a506709c75320a6e44bc910f280b
Ricardo Rodriguez <ricardoandres749@gmail.com>
2017-03-14 00:10:01 -0300

    about me 2.0

--- js/waypoints.js
+++ js/waypoints.js
@@ -1,9 +1,7 @@
 var $dipper = $('.dipper');
 $dipper.waypoint(function(){
-   $dipper.addClass('dipper-animation');
+          $dipper.addClass('dipper-animation');
+
 },{offset:'80%'});
 
-var $dipper2 = $('.dipper2');
-$dipper2.waypoint(function(){
-   $dipper2.addClass('dipper-animation2');
-},{offset:'90%'});
+
