6b5d8105827dfa0ae607cc90894abee4aa9c0609
Ricardo Rodriguez <ricardoandres749@gmail.com>
2017-03-22 19:27:04 -0300

    header for desktop and phone

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
+
 $(function(){
      
  
 
-        var header = document.getElementById('header');
+        /*var header = document.getElementById('header');
         
         var headroom  = new Headroom(header);
         
-        headroom.init(); 
+        headroom.init(); */
 
 
 });
    
     $(document).ready(function(){
-        
+      
+            
+              
+        $(window).scroll(function() {
+          if($(document).scrollTop() > 15 && $(window).width() > 767) {
+            $('#header').addClass('shrink');
+            $('#header').css('transition','all 0.4s ease');
+            $('#header').css('background', '#fff');
+            $('.navbar-inverse .navbar-nav > li > a').css('color','#5C5858');
+            $('#header').css('box-shadow','0px 0px 3px gray');
+            $('.navbar-inverse .navbar-brand ').css('color','#333');
+         
+            }
+            else {
+            $('#header').removeClass('shrink');
+            $('#header').css('transition','all 0.4s ease');
+            $('#header').css('background', 'transparent');
+            $('#header').css('box-shadow','none');
+            $('.navbar-inverse .navbar-nav > li > a').css('color','#fff');
+            $('.navbar-inverse .navbar-brand ').css('color','#fff');
+            }   
+      });
         
 
         $('.ir-arriba').click(function(){
@@ -24,7 +46,9 @@ $(function(){
         $(window).scroll(function(){
                 if($(this).scrollTop() > 0){
                       $('.ir-arriba').slideDown(300);
-                }else{
+                   
+                }else{  
+                 
                       $('.ir-arriba').slideUp(300);
                 }
         });
@@ -55,8 +79,10 @@ $(function(){
     });
 
 
+      
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
+
     $('#navegacion-rr a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
